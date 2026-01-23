cba74133997da8dff144e8d619dfe62d31399dad
xiaohaoxing <xiaohaoxing@users.noreply.github.com>
2026-01-23 04:19:51 +0000

    deploy: b45667ea24241316e082ec0aa542ebf55d31544e

---
+++ assets/collections.tDd4t8av.js
@@ -0,0 +1,1 @@
+import"./chunks/@vueuse/motion.DExMrowK.js";import{u as l}from"./chunks/vue-router.BNn-cICo.js";import{A as t}from"./framework.BfPwxjTV.js";const e=[];function a(){return{collections:t(()=>e)}}function p(){const o=l(),n=t(()=>o.path.startsWith("/collections/")?o.path.split("/")[2]:"");return{collection:t(()=>e.find(c=>c.key===n.value))}}export{a,p as u};
