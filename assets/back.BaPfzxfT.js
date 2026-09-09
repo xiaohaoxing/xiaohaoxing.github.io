f58df99b1e133c86e6d9868abc22b437ca68dab2
xiaohaoxing <xiaohaoxing@users.noreply.github.com>
2026-09-09 08:08:07 +0000

    deploy: d14a75f3569e966f0c2f9e0fd7ac1a0dff9e065c

---
+++ assets/back.BaPfzxfT.js
@@ -0,0 +1,1 @@
+import{a as o}from"./chunks/vue-router.CPRH3R7p.js";function u(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{u};
